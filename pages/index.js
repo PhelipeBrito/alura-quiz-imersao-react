9e1f7cff2d9e0e113df9c7cbd24fb476a220356e
Phelipe de Brito Silva <69465563+PhelipeBrito@users.noreply.github.com>
2021-01-25 17:26:42 -0300

    Código da primeira aula da imersão react

---
+++ pages/index.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+export default function Home() {
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+              <h1>The legend of Coders</h1>
+            </Widget.Header>
+          <Widget.Content>
+            
+            
+
+            <p>lorem ipsum sit dolor...</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da galera</h1>
+
+            <p>lorem ipsum sit dolor...</p>
+          </Widget.Content>
+        </Widget>
+
+        <Footer/>
+        <GitHubCorner/>
+      </QuizContainer>
+    </QuizBackground>
+  )
+}
