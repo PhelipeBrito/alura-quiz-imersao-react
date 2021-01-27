606bfbc39f3e60d16ae59b12c22c03f4cae2d788
Phelipe de Brito Silva <69465563+PhelipeBrito@users.noreply.github.com>
2021-01-27 15:50:09 -0300

    Ajuste nas metatags

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,9 @@ const PageHead = () => (
     <title>Aluraquiz — Black Clover</title>
     <meta property="og:title" content="Aluraquiz — Black Clover" key="title" />
     <meta property="og:image" content={db.bg} />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://alura-quiz-imersao-react.phelipebrito.vercel.app/" />
+    <meta property="og:description" content="Um quiz para testar seus conhecimentos do universo Black Clover e se divertir!" />
   </Head>
 );
 
